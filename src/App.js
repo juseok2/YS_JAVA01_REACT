982cf6f55afe430189f3185da392d2e1019faa63
juseok2 <wntjrwnstjr@naver.com>
2024-07-16 15:10:34 +0900

    step002_jsx표기법 시작

--- src/App.js
+++ src/App.js
@@ -4,20 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          리액트 배우기
-        </a>
-      </header>
+      <ul>
+        <li>강아지</li>
+        <li>고양이</li>
+        <li>토끼</li>
+        <li>거북이</li>
+      </ul>
+
+      <table border ='1px'>
+        <tr>
+          <td>이름</td>
+          <td>나이</td>
+        </tr>
+        <tr>
+          <td>홍길동</td>
+          <td>24</td>
+        </tr>
+        <tr>
+          <td>이순신</td>
+          <td>60</td>
+        </tr>
+      </table>
     </div>
   );
 }
